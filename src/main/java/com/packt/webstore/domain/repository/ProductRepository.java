e828901dff2c6c818baebde318e698a1449c2982
ArturKrotiuk <arturkrotiuk@gmail.com>
2017-01-07 17:45:47 +0100

    First commit

---
+++ src/main/java/com/packt/webstore/domain/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.packt.webstore.domain.repository;
+import com.packt.webstore.domain.Product;
+import java.util.List;
+
+public interface ProductRepository {
+	List <Product> getAllProducts();
+}
